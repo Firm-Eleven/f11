bb1b08a5097f773c6da89c7d72e7a4ca178891b4
AbdulHanan <abdul.hk471@gmail.com>
2026-06-05 13:37:23 +0200

    json updates

---
+++ f11/daybook/doctype/daybook_settings/daybook_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Zeeshan Afzal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Daybook Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
